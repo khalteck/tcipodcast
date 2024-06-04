01601ee84dd359aa0c8fecc14f84e7ce69702d3b
khalteck <khalidoyeneye@gmail.com>
2024-06-04 21:00:02 +0100

    feat: layout in progress x2

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -5,7 +5,7 @@ const Loader = () => {
   return (
     <div
       data-testid="loader"
-      className="w-full h-[100dvh] fixed top-0 left-0 bg-neutral-100 text-primary center-flex z-[100]"
+      className="w-full h-[100dvh] fixed top-0 left-0 bg-primary1/90 text-secondary center-flex z-[100]"
     >
       <LoaderMessageCard />
     </div>
--- src/components/common/LoaderMessageCard.jsx
+++ src/components/common/LoaderMessageCard.jsx
@@ -7,7 +7,7 @@ const LoaderMessageCard = () => {
         One moment please, <br />
         we are downloading some good vibes…
       </p>
-      <BeatLoader color={"#481297"} size={60} />
+      <BeatLoader color={"#ffc65b"} size={60} />
     </div>
   );
 };
